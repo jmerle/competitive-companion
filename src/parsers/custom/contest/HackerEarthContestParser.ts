3751b6395cfa2a85c3f499c25be3a1582bb76c8f
Jasper van Merle <jaspervmerle@gmail.com>
2017-12-31 13:50:11 +0100

    Fixed HackerEarth contest parser

--- src/parsers/custom/contest/HackerEarthContestParser.ts
+++ src/parsers/custom/contest/HackerEarthContestParser.ts
@@ -4,7 +4,7 @@ import HackerEarthProblemParser from "../problem/HackerEarthProblemParser";
 
 export default class HackerEarthContestParser extends ContestParser {
   problemParser: Parser = new HackerEarthProblemParser();
-  linkSelector: string = '.problems-table .prob a';
+  linkSelector: string = '.problems-table .prob a, .problems-table a.track-problem-link';
 
   getMatchPatterns(): string[] {
     return ['https://www.hackerearth.com/challenge/*/problems/'];
