0e652949cc6b3faf9402aa2535a697b2a21fe78e
Jasper van Merle <jaspervmerle@gmail.com>
2018-04-18 00:24:58 +0200

    Improved, Chrome-compatible options page

--- src/hosts/CustomHost.ts
+++ src/hosts/CustomHost.ts
@@ -9,6 +9,9 @@ export class CustomHost implements Host {
     return new Promise((resolve, reject) => {
       axios.post(`http://localhost:${this.port}/`, data, {
         timeout: 3000,
+        headers: {
+          'Content-Type': 'application/json',
+        },
       }).then(() => resolve()).catch(reject);
     });
   }
--- src/options.ts
+++ src/options.ts
@@ -7,29 +7,36 @@ let timeout: any = -1;
 function save() {
   const ports = customPortsInput()
     .value
-    .split(' ')
-    .filter(x => x.trim().length > 0)
+    .split(',')
+    .map(x => x.trim())
+    .filter(x => x.length > 0)
     .map(x => parseInt(x, 10));
 
-  if (ports.some(isNaN)) {
-    alert('Please make sure all ports are numeric, and multiple ports are separated by spaces.');
+  const uniquePorts = [...new Set(ports)];
+
+  if (uniquePorts.some(isNaN)) {
+    setStatus('Please make sure all ports are valid, and multiple ports are separated by commas.');
   } else {
-    Config.set('customPorts', ports).then(() => {
+    Config.set('customPorts', uniquePorts).then(() => {
       if (timeout === -1) {
-        (document.querySelector('#saved') as HTMLElement).style.visibility = 'visible';
+        setStatus('Saved!');
 
         timeout = setTimeout(() => {
           timeout = -1;
-          (document.querySelector('#saved') as HTMLElement).style.visibility = 'hidden';
+          setStatus('');
         }, 1500);
       }
     }).catch(console.error);
   }
 }
 
+function setStatus(status: string) {
+  document.querySelector('#status').textContent = status;
+}
+
 function load() {
   Config.get<number[]>('customPorts', []).then(ports => {
-    customPortsInput().value = ports.join(' ');
+    customPortsInput().value = ports.join(',');
   }).catch(console.error);
 }
 
