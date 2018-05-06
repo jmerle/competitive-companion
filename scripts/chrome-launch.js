c97204584e85474f76a379331885f79611dc4746
Jasper van Merle <jaspervmerle@gmail.com>
2018-05-06 22:04:37 +0200

    Update links

--- scripts/chrome-launch.js
+++ scripts/chrome-launch.js
@@ -1,5 +1,5 @@
 const chromeLaunch = require('chrome-launch');
 
 chromeLaunch('http://codeforces.com/problemset/problem/954/G', {
-  args: ['--start-maximized', '--load-extension=./build']
+  args: ['--start-maximized', '--load-extension=./build'],
 });
