1e15db6178edc2faceaa5dc6383daf467578d8de
Jasper van Merle <jaspervmerle@gmail.com>
2018-05-29 00:49:29 +0200

    Attempt to fix Travis

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,6 @@ module.exports = {
   exitOnPageError: false,
   launch: {
     headless: process.env.HEADLESS !== 'false',
-    args: ['--start-maximized'],
+    args: ['--start-maximized', '--no-sandbox'],
   },
 };
