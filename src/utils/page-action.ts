4c5f757dab969ba5bfd51a13347a79b780d34d71
Jasper van Merle <jaspervmerle@gmail.com>
2018-08-16 02:08:06 +0200

    Simplify page-action.ts

--- src/utils/page-action.ts
+++ src/utils/page-action.ts
@@ -1,23 +1,15 @@
 import { sendToBackground } from './messaging';
 import { MessageAction } from '../models/messaging';
 
-declare global {
-  const chrome: any;
-}
-
 function isContentScript(): boolean {
   return (window as any).isContentScript === true;
-}
-
-function isChrome(): boolean {
-  return window.navigator.userAgent.toLowerCase().includes('chrome');
 }
 
 export function enablePageAction(tabId?: number) {
   if (isContentScript()) {
     sendToBackground(MessageAction.EnablePageAction);
   } else {
-    (isChrome() ? chrome : browser).pageAction.show(tabId);
+    browser.pageAction.show(tabId);
   }
 }
 
@@ -25,6 +17,6 @@ export function disablePageAction(tabId?: number) {
   if (isContentScript()) {
     sendToBackground(MessageAction.DisablePageAction);
   } else {
-    (isChrome() ? chrome : browser).pageAction.hide(tabId);
+    browser.pageAction.hide(tabId);
   }
 }
