c6c3d11c97f99b6d589786309180725c6d88ee43
Jasper van Merle <jaspervmerle@gmail.com>
2019-02-02 01:43:40 +0100

    Improve Bloomberg parser and run Prettier on it

--- src/parsers/problem/BloombergCodeConProblemParser.ts
+++ src/parsers/problem/BloombergCodeConProblemParser.ts
@@ -5,12 +5,7 @@ import { Parser } from '../Parser';
 
 export class BloombergCodeConProblemParser extends Parser {
   public getMatchPatterns(): string[] {
-    return [
-      'https://codecon.bloomberg.com/*/*',
-      'http://codecon.bloomberg.com/*/*',
-      'https://codecon.bloomberg.com/contest/*/*',
-      'http://codecon.bloomberg.com/contest/*/*',
-    ];
+    return ['https://codecon.bloomberg.com/*/*', 'https://codecon.bloomberg.com/contest/*/*'];
   }
 
   public parse(url: string, html: string): Promise<Sendable> {
@@ -27,49 +22,38 @@ export class BloombergCodeConProblemParser extends Parser {
     task.setName(elem.querySelector('.problem-page-pane > h1').textContent.trim());
     task.setGroup(elem.querySelector('.sidebar-title').textContent.trim());
 
-    // task.setInteractive(
-    //   [...elem.querySelectorAll('.section-title')].some(
-    //     el => el.textContent === 'Interaction' || el.textContent === 'Протокол взаимодействия',
-    //   ),
-    // );
-
     const timeLimitStr = elem
       .querySelector('.problem-parameters > .fa-dashboard')
-      .childNodes[0].textContent.split(' ')[2].split('s')[0];
+      .childNodes[0].textContent.split(' ')[2]
+      .split('s')[0];
     task.setTimeLimit(parseFloat(timeLimitStr) * 1000);
 
     const memoryLimitStr = elem
       .querySelector('.fa-flash')
-      .childNodes[0].textContent.split(' ')[2].split('MB')[0];
+      .childNodes[0].textContent.split(' ')[2]
+      .split('MB')[0];
     task.setMemoryLimit(parseInt(memoryLimitStr, 10));
 
-    let inputs: any[] =[];
-    let outputs: any[]= [];
+    const inputs: string[] = [];
+    const outputs: string[] = [];
 
+    elem.querySelectorAll('.io-panel > .panel-item').forEach(item => {
+      const text = item.childNodes[1].textContent;
 
-    
-    elem
-      .querySelectorAll('.io-panel > .panel-item')
-      .forEach(o => 
-        {
-          const text = o.childNodes[1].textContent;
-          if(text=='Input') 
-          {
-            inputs=inputs.concat(o.childNodes[3].textContent);
-          }
-          else if (text=='Output')
-          {
-            outputs=outputs.concat(o.childNodes[3].textContent);
-          }
-        }
-      );
+      if (text === 'Input') {
+        inputs.push(item.childNodes[3].textContent);
+      } else if (text === 'Output') {
+        outputs.push(item.childNodes[3].textContent);
+      }
+    });
+
     for (let i = 0; i < inputs.length; i++) {
-      const input =  inputs[i];
+      const input = inputs[i];
       const output = outputs[i];
 
       task.addTest(input, output);
     }
 
-    task.setJavaMainClass("Problem");
+    task.setJavaMainClass('Problem');
   }
 }
