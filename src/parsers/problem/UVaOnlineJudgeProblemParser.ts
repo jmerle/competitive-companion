d25749c836b7b999bff8bed014d58b4afc726e90
Jasper van Merle <jaspervmerle@gmail.com>
2020-12-14 17:33:59 +0100

    Support external UVa Online Judge and ICPC Live Archive PDFs

--- src/parsers/problem/UVaOnlineJudgeProblemParser.ts
+++ src/parsers/problem/UVaOnlineJudgeProblemParser.ts
@@ -6,50 +6,68 @@ import { Parser } from '../Parser';
 
 export class UVaOnlineJudgeProblemParser extends Parser {
   public getMatchPatterns(): string[] {
-    return ['https://onlinejudge.org/index.php*', 'https://icpcarchive.ecs.baylor.edu/index.php*'];
+    return [
+      'https://onlinejudge.org/index.php*',
+      'https://icpcarchive.ecs.baylor.edu/index.php*',
+      'https://onlinejudge.org/external/*/*.pdf',
+      'https://icpcarchive.ecs.baylor.edu/external/*/*.pdf',
+    ];
   }
 
   public getRegularExpressions(): RegExp[] {
     return [
       /https:\/\/onlinejudge\.org\/index\.php\?(.*)page=show_problem(.*)problem=(\d+)(.*)/,
       /https:\/\/icpcarchive\.ecs\.baylor\.edu\/index\.php\?(.*)page=show_problem(.*)problem=(\d+)(.*)/,
+      /https:\/\/onlinejudge\.org\/external\/(.*)\.pdf/,
+      /https:\/\/icpcarchive\.ecs\.baylor\.edu\/external\/(.*)\.pdf/,
     ];
   }
 
   public async parse(url: string, html: string): Promise<Sendable> {
-    const elem = htmlToElement(html);
-
-    const container = elem.querySelector('#col3_content_wrapper, td.main');
-
-    const isUVa = !container.classList.contains('main');
+    const isUVa = url.includes('onlinejudge.org');
     const task = new TaskBuilder(isUVa ? 'UVa Online Judge' : 'ICPC Live Archive').setUrl(url);
 
-    const header = container.querySelector('h3');
-    const iframe = container.querySelector('iframe');
-
-    task.setName(header.textContent);
-
-    task.setTimeLimit(parseFloat(/Time limit: ([0-9.]+) seconds/.exec(header.nextSibling.textContent)[1]) * 1000);
-    task.setMemoryLimit(32);
-
-    try {
-      const iframeUrl = iframe.src;
-
-      const firstPart = /(.*)\//.exec(iframeUrl)[1];
-      const secondPart = /(?:.*)\/(.*)\.html/.exec(iframeUrl)[1];
-      const pdfUrl = firstPart + '/p' + secondPart + '.pdf';
-
-      await this.parseTestsFromPdf(task, pdfUrl);
-    } catch (err) {
-      // Do nothing
+    if (url.endsWith('.pdf')) {
+      await this.parseFromPdf(task, url);
+    } else {
+      await this.parseFromHtml(task, html);
     }
 
     return task.build();
   }
 
-  public async parseTestsFromPdf(task: TaskBuilder, pdfUrl: string): Promise<void> {
+  private async parseFromHtml(task: TaskBuilder, html: string): Promise<void> {
+    const elem = htmlToElement(html);
+    const container = elem.querySelector('#col3_content_wrapper, td.main');
+
+    const header = container.querySelector('h3');
+    task.setName(header.textContent);
+
+    task.setTimeLimit(parseFloat(/Time limit: ([0-9.]+) seconds/.exec(header.nextSibling.textContent)[1]) * 1000);
+    task.setMemoryLimit(32);
+
+    const iframe = container.querySelector('iframe');
+    const iframeUrl = iframe.src;
+
+    const firstPart = /(.*)\//.exec(iframeUrl)[1];
+    const secondPart = /(?:.*)\/(.*)\.html/.exec(iframeUrl)[1];
+    const pdfUrl = firstPart + '/p' + secondPart + '.pdf';
+
     const lines = await readPdf(pdfUrl);
 
+    await this.parseTestsFromPdf(task, lines);
+  }
+
+  private async parseFromPdf(task: TaskBuilder, pdfUrl: string): Promise<void> {
+    const lines = await readPdf(pdfUrl);
+
+    const problemName = lines[1][0] === '\\' ? '"' + lines[1].substr(1) : lines[1];
+    task.setName(`${lines[0]} - ${problemName}`);
+
+    await this.parseTestsFromPdf(task, lines);
+  }
+
+  private async parseTestsFromPdf(task: TaskBuilder, lines: string[]): Promise<void> {
     const interactiveKeywords = ['interaction protocol', 'sample interaction'];
     task.setInteractive(lines.some(line => interactiveKeywords.indexOf(line.toLowerCase()) > -1));
 
@@ -57,7 +75,13 @@ export class UVaOnlineJudgeProblemParser extends Parser {
     const outputStart = lines.findIndex(line => line.toLowerCase() === 'sample output');
 
     if (inputStart !== -1 && outputStart !== -1) {
-      const input = lines.slice(inputStart + 1, outputStart).join('\n');
+      let inputLines = lines.slice(inputStart + 1, outputStart);
+
+      if (/(\d+)\/(\d+)/.test(inputLines[inputLines.length - 1])) {
+        inputLines = inputLines.slice(0, inputLines.length - 3);
+      }
+
+      const input = inputLines.join('\n');
       const output = lines.slice(outputStart + 1).join('\n');
 
       task.addTest(input, output);
