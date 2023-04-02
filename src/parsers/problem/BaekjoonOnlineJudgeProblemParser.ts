8ec7495ac06965d3aa2e41046def40e81ad2471c
Jasper van Merle <jaspervmerle@gmail.com>
2023-04-02 13:48:47 +0200

    Fix Baekjoon Online Judge parser for problems with multiple time limits

--- src/parsers/problem/BaekjoonOnlineJudgeProblemParser.ts
+++ src/parsers/problem/BaekjoonOnlineJudgeProblemParser.ts
@@ -15,7 +15,20 @@ export class BaekjoonOnlineJudgeProblemParser extends Parser {
     task.setName(elem.querySelector('#problem_title').textContent);
 
     const constraintCells = elem.querySelectorAll('#problem-info > tbody > tr > td');
-    task.setTimeLimit(parseFloat(/([0-9.]+) /.exec(constraintCells[0].textContent)[1]) * 1000);
+
+    if (/\d/.test(constraintCells[0].textContent)) {
+      task.setTimeLimit(Math.floor(parseFloat(/([0-9.]+) /.exec(constraintCells[0].textContent)[1]) * 1000));
+    } else {
+      const timeLimits = [...elem.querySelectorAll('#problem-body p')]
+        .map(el => el.textContent.trim())
+        .filter(text => text.startsWith('시간 제한:'))
+        .map(text => parseInt(/(\d+)/.exec(text)[1]) * 1000);
+
+      if (timeLimits.length > 0) {
+        task.setTimeLimit(Math.max(...timeLimits));
+      }
+    }
+
     task.setMemoryLimit(parseInt(/(\d+) /.exec(constraintCells[1].textContent)[1], 10));
 
     const inputs = elem.querySelectorAll('pre[id^="sample-input-"]');
