8ca8e33f4014c3d920b5a9961e1aea6bfea860c0
Jasper van Merle <jaspervmerle@gmail.com>
2023-10-05 01:50:04 +0200

    Minor refactoring in scripts/start-chrome.ts

--- scripts/start-chrome.ts
+++ scripts/start-chrome.ts
@@ -1,14 +1,16 @@
+import * as process from 'process';
 import * as ChromeLauncher from 'chrome-launcher';
 
-(async () => {
-  await ChromeLauncher.launch({
-    startingUrl: 'https://codeforces.com/problemset/problem/954/G',
-    ignoreDefaultFlags: true,
-    chromeFlags: [
-      '--no-first-run',
-      '--no-default-browser-check',
-      '--start-maximized',
-      '--load-extension=./build-extension',
-    ],
-  });
-})();
+ChromeLauncher.launch({
+  startingUrl: 'https://codeforces.com/problemset/problem/954/G',
+  ignoreDefaultFlags: true,
+  chromeFlags: [
+    '--no-first-run',
+    '--no-default-browser-check',
+    '--start-maximized',
+    '--load-extension=./build-extension',
+  ],
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
+});
