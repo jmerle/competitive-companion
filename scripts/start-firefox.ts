0b1c51c926f9c91134460dd158f635cfbdfe4fdf
Jasper van Merle <jaspervmerle@gmail.com>
2023-10-04 21:44:15 +0200

    Fix start:firefox script on Ubuntu with snap-packaged Firefox

---
+++ scripts/start-firefox.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as process from 'process';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as webExt from 'web-ext';
+
+const projectRoot = path.dirname(__dirname);
+const extensionDir = path.join(projectRoot, 'build-extension');
+const tmpDir = path.join(projectRoot, 'firefox-tmp');
+
+if (fs.existsSync(tmpDir)) {
+  fs.rmSync(tmpDir, { recursive: true, force: true });
+}
+
+fs.mkdirSync(tmpDir);
+process.env.TMPDIR = tmpDir;
+
+webExt.cmd
+  .run(
+    {
+      sourceDir: extensionDir,
+      startUrl: 'https://codeforces.com/problemset/problem/954/G',
+      pref: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'devtools.browserconsole.filter.jswarn': 'false',
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'devtools.webconsole.filter.warn': 'false',
+      },
+    },
+    { shouldExitProgram: false },
+  )
+  .catch((err: any) => {
+    console.error(err);
+    process.exit(1);
+  });
