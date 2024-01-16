56f5377d0d0a038df8a745192664249692f1ccd4
Nikola Stojkovski <92634082+EntityPlantt@users.noreply.github.com>
2024-01-16 19:11:28 +0100

    Follow lint standards

--- src/parsers/problem/MendoProblemParser.ts
+++ src/parsers/problem/MendoProblemParser.ts
@@ -1,7 +1,7 @@
-import { Sendable } from "../../models/Sendable";
-import { TaskBuilder } from "../../models/TaskBuilder";
-import { htmlToElement } from "../../utils/dom";
-import { Parser } from "../Parser";
+import { Sendable } from '../../models/Sendable';
+import { TaskBuilder } from '../../models/TaskBuilder';
+import { htmlToElement } from '../../utils/dom';
+import { Parser } from '../Parser';
 
 export class MendoProblemParser extends Parser {
   public getMatchPatterns(): string[] {
@@ -14,41 +14,42 @@ export class MendoProblemParser extends Parser {
     const elem = htmlToElement(html);
     const task = new TaskBuilder('Mendo').setUrl(url);
 
-    task.setName(elem.querySelector(".pagetitle").textContent);
-    
+    task.setName(elem.querySelector('.pagetitle').textContent);
+
     elem.querySelectorAll('.taskContentView > h3').forEach(x => {
-      if (x.textContent.trim() == "Ограничувања") {
+      if (x.textContent.trim() == 'Ограничувања') {
         // Македонски
-        var results =
-          /Временско ограничување: (\d+) ([а-ш]+)Мемориско ограничување: (\d+) ([а-шј]+)/s
-          .exec(x.nextElementSibling.textContent);
-        var time = parseInt(results[1]), space = parseInt(results[3]);
-        if (results[2].slice(0, 6) == "секунд") time *= 1000;
+        const results = /Временско ограничување: (\d+) ([а-ш]+)Мемориско ограничување: (\d+) ([а-шј]+)/s.exec(
+          x.nextElementSibling.textContent,
+        );
+        let time = parseInt(results[1]);
+        const space = parseInt(results[3]);
+        if (results[2].slice(0, 6) == 'секунд') time *= 1000;
         // За сега нема задачи со <1MB мемориски лимит.
         task.setTimeLimit(time).setMemoryLimit(space);
       }
-      if (x.textContent.trim() == "Constraints") {
+      if (x.textContent.trim() == 'Constraints') {
         // English
-        var results =
-        /Time limit: (\d+) ([a-z]+)Memory limit: (\d+) ([a-z]+)/s
-          .exec(x.nextElementSibling.textContent);
-        var time = parseInt(results[1]), space = parseInt(results[3]);
-        if (results[2].slice(0, 6) == "second") time *= 1000;
+        const results = /Time limit: (\d+) ([a-z]+)Memory limit: (\d+) ([a-z]+)/s.exec(
+          x.nextElementSibling.textContent,
+        );
+        let time = parseInt(results[1]);
+        const space = parseInt(results[3]);
+        if (results[2].slice(0, 6) == 'second') time *= 1000;
         // As of now there aren't any problems with <1MB space limit.
         task.setTimeLimit(time).setMemoryLimit(space);
       }
     });
-    if (elem.querySelector(".taskContentView tbody")) {
-      elem.querySelector(".taskContentView tbody").childNodes.forEach(x => {
-        var parsed = /^(?:input|влез)\n(.*)(?:output|излез)\n(.*)$/s.exec(x.textContent);
-        task.addTest(parsed[1] + "\n", parsed[2] + "\n");
+    if (elem.querySelector('.taskContentView tbody')) {
+      elem.querySelector('.taskContentView tbody').childNodes.forEach(x => {
+        const parsed = /^(?:input|влез)\n(.*)(?:output|излез)\n(.*)$/s.exec(x.textContent);
+        task.addTest(parsed[1] + '\n', parsed[2] + '\n');
       });
-    }
-    else {
+    } else {
       // As of now there isn't a better discovered way of checking if it's interactive :rofl:
       task.setInteractive(true);
     }
-    task.setJavaMainClass("Main");
+    task.setJavaMainClass('Main');
     return task.build();
   }
 }
