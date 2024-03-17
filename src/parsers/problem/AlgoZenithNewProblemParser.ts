7d3a910633bb4ad5d29c300ab19840feb5327a47
Jasper van Merle <jaspervmerle@gmail.com>
2024-03-17 20:17:29 +0100

    Make AlgoZenithNewProblemParser's selectors more robust

--- src/parsers/problem/AlgoZenithNewProblemParser.ts
+++ src/parsers/problem/AlgoZenithNewProblemParser.ts
@@ -14,19 +14,22 @@ export class AlgoZenithNewProblemParser extends Parser {
 
     task.setName(elem.querySelector('h4').textContent);
 
-    const timeLimitStr =
-      elem.querySelector('h4').parentElement.parentElement.childNodes[1].childNodes[1].childNodes[0].textContent;
+    const timeLimitStr = this.getLimit(elem, 'Time Limit');
     task.setTimeLimit(parseInt(/(\d+)/.exec(timeLimitStr)[1]) * 1000);
 
-    const memoryLimitStr =
-      elem.querySelector('h4').parentElement.parentElement.childNodes[1].childNodes[2].childNodes[0].textContent;
+    const memoryLimitStr = this.getLimit(elem, 'Memory');
     task.setMemoryLimit(parseInt(/(\d+)/.exec(memoryLimitStr)[1]) / 1000);
 
-    const blocks = elem.querySelectorAll('.mt-4 .coding_input_format__pv9fS');
+    const blocks = elem.querySelectorAll('.mt-4 div[class^="coding_input_format__"]');
     for (let i = 0; i < blocks.length - 1; i += 2) {
       task.addTest(blocks[i].textContent, blocks[i + 1].textContent);
     }
 
     return task.build();
+  }
+
+  private getLimit(elem: Element, label: string): string {
+    const labelElem = [...elem.querySelectorAll('.dmsans')].find(el => el.textContent.trim() === label);
+    return labelElem.previousElementSibling.textContent;
   }
 }
