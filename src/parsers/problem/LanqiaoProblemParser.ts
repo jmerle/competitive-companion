62b4d16fc829960d70a1e8dc3a03ada73401fcb7
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-04-08 21:48:22 +0600

    fix liniting issues

--- src/parsers/problem/LanqiaoProblemParser.ts
+++ src/parsers/problem/LanqiaoProblemParser.ts
@@ -12,18 +12,18 @@ export class LanqiaoProblemParser extends Parser {
     const doc = htmlToElement(html);
     const task = new TaskBuilder('Some OJ').setUrl(url);
 
-    const title = document.querySelector('.course-name').textContent;
+    const title = doc.querySelector('.course-name').textContent;
     task.setName(title.replace(/\s+/g, ' ').trim());
 
-    const limitStr = document.querySelector('#运行限制 + table > tbody > tr').textContent;
+    const limitStr = doc.querySelector('#运行限制 + table > tbody > tr').textContent;
     const [cpu, memory] = limitStr.match(/\d+/g);
     task.setTimeLimit(parseFloat(cpu) * 1000);
     task.setMemoryLimit(parseInt(memory, 10));
 
     // The problems on this OJ seems to have only one test case.
     // No counter-example found.
-    const input = document.querySelector('#输入样例 + pre > code, #样例输入 + pre > code').textContent;
-    const output = document.querySelector('#输出样例 + pre > code, #样例输出 + pre > code').textContent;
+    const input = doc.querySelector('#输入样例 + pre > code, #样例输入 + pre > code').textContent;
+    const output = doc.querySelector('#输出样例 + pre > code, #样例输出 + pre > code').textContent;
     task.addTest(input, output);
 
     return task.build();
