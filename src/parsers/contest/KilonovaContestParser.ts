f5da3ab9ebb03fbe2ea015f26431afced89154d5
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-05-07 00:54:12 +0300

    Shorten the link selector on KilonovaContestParser

--- src/parsers/contest/KilonovaContestParser.ts
+++ src/parsers/contest/KilonovaContestParser.ts
@@ -2,20 +2,12 @@ import { SimpleContestParser } from '../SimpleContestParser';
 import { KilonovaProblemParser } from '../problem/KilonovaProblemParser';
 
 export class KilonovaContestParser extends SimpleContestParser {
-    protected linkSelector = 'div.reset-list > ul > li > a[href^="/contests"]';
+    protected linkSelector = 'div.reset-list > ul > li > a[href^="/"]';
     protected problemParser = new KilonovaProblemParser();
 
     public getMatchPatterns(): string[] {
         return [
-            'https://kilonova.ro/problems/*',
-            'https://kilonova.ro/contests/*/problems/*',
-        ];
-    }
-
-    public getRegularExpressions(): RegExp[] {
-        return [
-            /https:\/\/kilonova[.]ro\/contests\/(\d+)\/problems\/(\d+)$/,
-            /https:\/\/kilonova[.]ro\/problems\/(\d+)$/
+            'https://kilonova.ro/*problems/*',
         ];
     }
 
