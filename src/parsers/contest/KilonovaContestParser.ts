48ad64705bdd9e5961f566f5381c85129d670229
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-05-07 00:11:27 +0300

    Fix KilonovaContestParser link selector

--- src/parsers/contest/KilonovaContestParser.ts
+++ src/parsers/contest/KilonovaContestParser.ts
@@ -2,7 +2,7 @@ import { SimpleContestParser } from '../SimpleContestParser';
 import { KilonovaProblemParser } from '../problem/KilonovaProblemParser';
 
 export class KilonovaContestParser extends SimpleContestParser {
-    protected linkSelector = 'details.segment-panel > div.reset-list > ul > li > a';
+    protected linkSelector = 'div.reset-list > ul > li > a[href^="/contests"]';
     protected problemParser = new KilonovaProblemParser();
 
     public getMatchPatterns(): string[] {
@@ -11,5 +11,13 @@ export class KilonovaContestParser extends SimpleContestParser {
             'https://kilonova.ro/contests/*/problems/*',
         ];
     }
+
+    public getRegularExpressions(): RegExp[] {
+        return [
+            /https:\/\/kilonova[.]ro\/contests\/(\d+)\/problems\/(\d+)$/,
+            /https:\/\/kilonova[.]ro\/problems\/(\d+)$/
+        ];
+    }
+
 
 }
