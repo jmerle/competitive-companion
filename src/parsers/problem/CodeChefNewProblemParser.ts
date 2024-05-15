9e66462b232b8c8700563dbf5ec8a511e5b20453
Jasper van Merle <jaspervmerle@gmail.com>
2024-05-15 19:52:40 +0200

    Fix CodeChef problem title parsing

--- src/parsers/problem/CodeChefNewProblemParser.ts
+++ src/parsers/problem/CodeChefNewProblemParser.ts
@@ -21,7 +21,7 @@ export class CodeChefNewProblemParser extends Parser {
     task.setName(
       elem.querySelector(
         ['_problem__title_', '_contestProblemTitle_', '_titleStatus__container_', '_problemBody_']
-          .map(prefix => `div[class^="${prefix}"] > h1, div[class^="${prefix}"] > h2`)
+          .map(prefix => `div[class^="${prefix}"] > h1, div[class^="${prefix}"] > h2, div[class^="${prefix}"] > h3`)
           .join(', '),
       ).textContent,
     );
