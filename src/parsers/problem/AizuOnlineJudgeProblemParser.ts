e4c840eb854819ac949316079c557ddc0ea028e4
Jasper van Merle <jaspervmerle@gmail.com>
2025-05-29 07:21:33 +0200

    Support alternative Aizu Online Judge UI

--- src/parsers/problem/AizuOnlineJudgeProblemParser.ts
+++ src/parsers/problem/AizuOnlineJudgeProblemParser.ts
@@ -5,29 +5,46 @@ import { Parser } from '../Parser';
 
 export class AizuOnlineJudgeProblemParser extends Parser {
   public getMatchPatterns(): string[] {
-    return ['https://judge.u-aizu.ac.jp/onlinejudge/description.jsp*'];
+    return [
+      'https://judge.u-aizu.ac.jp/onlinejudge/description.jsp*',
+      'https://onlinejudge.u-aizu.ac.jp/courses/*',
+      'https://onlinejudge.u-aizu.ac.jp/challenges/*',
+    ];
   }
 
   public async parse(url: string, html: string): Promise<Sendable> {
     const elem = htmlToElement(html);
     const task = new TaskBuilder('Aizu Online Judge').setUrl(url);
 
-    task.setName(elem.querySelector('#problemTitle').textContent);
+    task.setName(elem.querySelector('h1').textContent);
 
-    const breadcrumbContainer = elem.querySelector('#pwd');
-    const breadcrumbs = [...breadcrumbContainer.querySelectorAll('.now')].map(el => el.textContent);
-    breadcrumbs.push(breadcrumbContainer.childNodes[breadcrumbContainer.childNodes.length - 1].textContent);
-    breadcrumbs.shift();
+    let breadcrumbs: string[];
+    let limitsStr: string;
+    let unfilteredBlocks: NodeListOf<Element>;
+    if (url.includes('//judge')) {
+      const breadcrumbContainer = elem.querySelector('#pwd');
+      breadcrumbs = [...breadcrumbContainer.querySelectorAll('.now')].map(el => el.textContent);
+      breadcrumbs.push(breadcrumbContainer.childNodes[breadcrumbContainer.childNodes.length - 1].textContent);
+      breadcrumbs.shift();
+
+      limitsStr = elem.querySelector('#pageinfo span.text-red3').textContent;
+      unfilteredBlocks = elem.querySelectorAll('.description > pre');
+    } else {
+      breadcrumbs = [...elem.querySelectorAll('.breadcrumbs .link')]
+        .map(el => el.textContent.trim())
+        .map(text => text[0].toUpperCase() + text.substring(1));
+      breadcrumbs.shift();
+
+      limitsStr = elem.querySelector('.problemInfo').textContent;
+      unfilteredBlocks = elem.querySelectorAll('.problemBody > pre');
+    }
 
     task.setCategory(breadcrumbs.join(' - '));
 
-    const timeLimitStr = elem.querySelector('#problemTimeLimit').textContent;
-    task.setTimeLimit(parseInt(timeLimitStr, 10) * 1000);
+    task.setTimeLimit(parseInt(/(\d+) sec/.exec(limitsStr)[1]) * 1000);
+    task.setMemoryLimit(parseInt(/(\d+) KB/.exec(limitsStr)[1]) / 1024);
 
-    const memoryLimitStr = elem.querySelector('#problemMemoryLimit').textContent;
-    task.setMemoryLimit(parseInt(memoryLimitStr, 10) / 1000);
-
-    const blocks = [...elem.querySelectorAll('.description > pre')].filter(el => {
+    const blocks = [...unfilteredBlocks].filter(el => {
       const previousElement = el.previousElementSibling;
 
       if (!previousElement || previousElement.tagName !== 'H2') {
@@ -35,10 +52,12 @@ export class AizuOnlineJudgeProblemParser extends Parser {
       }
 
       const headerText = previousElement.textContent.toLowerCase();
-      return ['ample input', 'ample output', '入力例', '出力例'].some(x => headerText.includes(x.toLowerCase()));
+      return ['ample input', 'ample output', '力例', '入力例', '出力例'].some(x =>
+        headerText.includes(x.toLowerCase()),
+      );
     });
 
-    for (let i = 0; i < blocks.length; i += 2) {
+    for (let i = 0; i < blocks.length - 1; i += 2) {
       task.addTest(blocks[i].textContent, blocks[i + 1].textContent);
     }
 
