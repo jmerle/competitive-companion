a38e24aae69a04823a41d6b2a6a096e755be71c3
kasinglee <kasinglee0602@gmail.com>
2025-09-21 02:17:58 +0800

    Fix parser bug of PTA

--- src/parsers/problem/PTAProblemParser.ts
+++ src/parsers/problem/PTAProblemParser.ts
@@ -12,22 +12,40 @@ export class PTAProblemParser extends Parser {
     const elem = htmlToElement(html);
     const task = new TaskBuilder('PTA').setUrl(url);
 
-    const container = elem.querySelector('div[class^="mn"] div[class^="left"]');
+    const container = elem.querySelector('.scroll');
 
-    task.setName(container.querySelector('span.font-bold').textContent.trim());
-    task.setCategory(elem.querySelector('.fixed.top-0.w-full .text-lg.ellipsis').textContent.trim());
+    task.setName(
+      container.querySelector('.text-darkest.font-bold.text-lg')!.textContent!.trim()
+    );
+    const categoryEl = elem.querySelector('.fixed.top-0.w-full .text-lg.ellipsis');
+    task.setCategory(categoryEl ? categoryEl.textContent!.trim() : '');
 
-    const limits = [...container.querySelectorAll('div[class*="problemInfo"] .pc-text-raw')].map(l => l.textContent);
-    const timeLimitStr = limits.find(text => text.includes('MB'));
-    const memoryLimitStr = limits.find(text => text.includes('ms'));
+    const limits = [...elem.querySelectorAll('.problemInfo_tfBoz .pc-text-raw')].map(
+      (l) => l.textContent || ''
+    );
+
+    const timeLimitStr = limits.find(text => text.includes('ms'));
+    const memoryLimitStr = limits.find(text => text.includes('MB'));
 
     task.setTimeLimit(parseInt(/(\d+)/.exec(timeLimitStr)[1], 10));
     task.setMemoryLimit(parseInt(/(\d+)/.exec(memoryLimitStr)[1], 10));
 
-    const blocks = [...container.querySelectorAll('.rendered-markdown > pre > code:not(.hljs)')];
+    const inputHeader = elem.querySelector('h3[id*="输入样例"]');
+    const outputHeader = elem.querySelector('h3[id*="输出样例"]');
+    
+    if (inputHeader && outputHeader) {
+      const inputCode = inputHeader.nextElementSibling?.querySelector('code')?.textContent?.trim();
+      const outputCode = outputHeader.nextElementSibling?.querySelector('code')?.textContent?.trim();
+      
+      if (inputCode && outputCode) {
+        task.addTest(inputCode, outputCode);
+      }
+    } else {
+      const blocks = [...elem.querySelectorAll('.rendered-markdown pre > code:not(.hljs)')];
 
-    for (let i = 0; i < blocks.length - 1; i += 2) {
-      task.addTest(blocks[i].textContent, blocks[i + 1].textContent);
+      for (let i = 0; i < blocks.length - 1; i += 2) {
+        task.addTest(blocks[i].textContent, blocks[i + 1].textContent);
+      }
     }
 
     return task.build();
