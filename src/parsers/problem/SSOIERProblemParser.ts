ef9575b08d7c755da956d76ab0f1c63c3af36d60
Jasper van Merle <jaspervmerle@gmail.com>
2026-03-15 21:16:31 +0100

    Format SSOIER selector

--- src/parsers/problem/SSOIERProblemParser.ts
+++ src/parsers/problem/SSOIERProblemParser.ts
@@ -16,7 +16,7 @@ export class SSOIERProblemParser extends Parser {
     const elem = htmlToElement(html);
     const task = new TaskBuilder('SSOIER').setUrl(url);
 
-    const container = elem.querySelector('body>center>center td');
+    const container = elem.querySelector('body > center > center td');
 
     task.setName(container.querySelector('h3').textContent);
 
