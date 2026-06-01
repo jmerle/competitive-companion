844223d7db88df1e0b63f2e058f9d62ae7cb0752
munjeokman <choith01234@gmail.com>
2026-06-01 17:09:31 +0900

    Add DOJ (dojoi.xyz) problem parser

--- src/parsers/parsers.ts
+++ src/parsers/parsers.ts
@@ -160,6 +160,7 @@ import { YandexProblemParser } from './problem/YandexProblemParser';
 import { YukicoderProblemParser } from './problem/YukicoderProblemParser';
 import { ZOJProblemParser } from './problem/ZOJProblemParser';
 import { ZUFEOJProblemParser } from './problem/ZUFEOJProblemParser';
+import { DOJProblemParser } from './problem/DOJProblemParser';
 
 export const parsers: Parser[] = [
   new A2OnlineJudgeProblemParser(),
@@ -425,4 +426,6 @@ export const parsers: Parser[] = [
 
   new ZUFEOJProblemParser(),
   new ZUFEOJContestParser(),
+
+  new DOJProblemParser(),
 ];
